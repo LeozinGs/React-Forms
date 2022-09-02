3d65545e258b9e8f4ea78854ae9b1d7f56cd57a0
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-02 12:38:49 -0300

    🎉

---
+++ src/components/MyForm.js
@@ -0,0 +1,59 @@
+import './MyForm.css'
+import {useState} from 'react'
+
+const MyForm = ({user}) => {
+
+  //6 Controlled inputs
+
+
+  //3 Gerenciamento de dados
+  const [name, setName] = useState(user ? user.name : "")
+  const [email, setEmail] = useState(user ? user.email : "")
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  // console.log(name)
+  // console.log(email)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("enviando o formulário")
+    console.log(name, email)
+  }
+
+  return (
+    <div>
+      {/* 5 Envio de form */}
+
+      {/* 1 Criação de Form */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nome: </label>
+          <input 
+            type="text"
+            name="name" 
+            placeholder="Digite seu nome"
+            onChange={handleName}
+            value={name}
+          />
+        </div>
+        {/* 2 Label envolvendo input */}
+        <label>
+          <span>E-mail: </span>
+          {/* 4 Simplificação de manipulação de state inline */}
+          <input 
+            type="email" 
+            name="email" 
+            placeholder="Digite seu E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <input type="submit" value="Enviar"/>
+      </form>
+    </div>
+  )
+}
+
+export default MyForm
